5e7c64f39da8d78a5b6a8805cc278b52cfef0e91
Elena Ilyukhina <elena.ilyukhina@hotmail.com>
2017-11-29 19:27:30 +0100

    while removed

--- L02.1-memory/src/main/java/ru/otus/l021/Main.java
+++ L02.1-memory/src/main/java/ru/otus/l021/Main.java
@@ -15,24 +15,21 @@ public class Main {
 
 		ObjectSizeMeasurer measurer = new ObjectSizeMeasurer();
 
-		System.out.println("Starting the loop");
-		while (true) {
-			System.out.println("Size of Integer: " + measurer.getObjectSize(() -> new Integer(5)));
-			System.out.println("Size of Long: " + measurer.getObjectSize(() -> new Long(5L)));
-			System.out.println("Size of Float: " + measurer.getObjectSize(() -> new Float(5.0f)));
-			System.out.println("Size of Double: " + measurer.getObjectSize(() -> new Double(5.0)));
-			System.out.println("Size of String: " + measurer.getObjectSize(() -> new String("")));
-			System.out.println("Size of String char[0]: " + measurer.getObjectSize(() -> new String(new char[0])));
-			System.out.println("Size of String abc: " + measurer.getObjectSize(() -> new String("abc")));
-			System.out.println("Size of String char[3] abc: " + measurer.getObjectSize(() -> new String(new char[] {'a', 'b', 'c'})));
-			System.out.println("Size of int[0]: " + measurer.getObjectSize(() -> new int[0]));
-			System.out.println("Size of int[10]: " + measurer.getObjectSize(() -> new int[10]));
-			System.out.println("Size of int[100]: " + measurer.getObjectSize(() -> new int[100]));
-			System.out.println("Size of ArrayList<Integer>(): " + measurer.getObjectSize(() -> new ArrayList<Integer>()));
-			System.out.println("Size of ArrayList<Integer>(10): " + measurer.getObjectSize(() -> new ArrayList<Integer>(Arrays.asList(new Integer[10]))));
-			System.out.println("Size of MyClass: " + measurer.getObjectSize(() -> new MyClass()));
-			System.out.println("Size of ObjectSizeMeasurer: " + measurer.getObjectSize(() -> new ObjectSizeMeasurer()));
-		}
+		System.out.println("Size of Integer: " + measurer.getObjectSize(() -> new Integer(5)));
+		System.out.println("Size of Long: " + measurer.getObjectSize(() -> new Long(5L)));
+		System.out.println("Size of Float: " + measurer.getObjectSize(() -> new Float(5.0f)));
+		System.out.println("Size of Double: " + measurer.getObjectSize(() -> new Double(5.0)));
+		System.out.println("Size of String: " + measurer.getObjectSize(() -> new String("")));
+		System.out.println("Size of String char[0]: " + measurer.getObjectSize(() -> new String(new char[0])));
+		System.out.println("Size of String abc: " + measurer.getObjectSize(() -> new String("abc")));
+		System.out.println("Size of String char[3] abc: " + measurer.getObjectSize(() -> new String(new char[] {'a', 'b', 'c'})));
+		System.out.println("Size of int[0]: " + measurer.getObjectSize(() -> new int[0]));
+		System.out.println("Size of int[10]: " + measurer.getObjectSize(() -> new int[10]));
+		System.out.println("Size of int[100]: " + measurer.getObjectSize(() -> new int[100]));
+		System.out.println("Size of ArrayList<Integer>(): " + measurer.getObjectSize(() -> new ArrayList<Integer>()));
+		System.out.println("Size of ArrayList<Integer>(10): " + measurer.getObjectSize(() -> new ArrayList<Integer>(Arrays.asList(new Integer[10]))));
+		System.out.println("Size of MyClass: " + measurer.getObjectSize(() -> new MyClass()));
+		System.out.println("Size of ObjectSizeMeasurer: " + measurer.getObjectSize(() -> new ObjectSizeMeasurer()));
 	}
 }	
 
